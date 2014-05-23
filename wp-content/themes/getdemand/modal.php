d6ec9a837702857cbda698ea2eb81e7045415be1
Cameron Scott <cameron.scott137@gmail.com>
2014-05-23 17:45:40 -0400

    Adds Sharpspring.

--- wp-content/themes/getdemand/modal.php
+++ wp-content/themes/getdemand/modal.php
@@ -8,6 +8,13 @@
       </div>
       <div class="modal-body">
         <?php echo do_shortcode('[gravityform id="6" name="Webinar" title="false" description="false" ajax="true"]');?>
+        <script type="text/javascript">
+    var __ss_noform = __ss_noform || [];
+    __ss_noform.push(['baseURI', 'https://app-NTJGC2.sharpspring.com/webforms/receivePostback/MzQ0MwYA/']);
+    __ss_noform.push(['endpoint', 'eb10c4bd-0695-4228-9961-26b942e68d52']);
+</script>
+<script type="text/javascript" src="https://koi-NTJGC2.sharpspring.com/client/noform.js?ver=1.0" ></script>
+
       </div>
     </div><!-- /.modal-content -->
   </div><!-- /.modal-dialog -->
