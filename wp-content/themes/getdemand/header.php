14e0d11cbc43633a820e23d8a38923b44521efcf
Cameron Scott <cameron.scott137@gmail.com>
2014-05-23 17:52:38 -0400

    Sharpspring tracking.

--- wp-content/themes/getdemand/header.php
+++ wp-content/themes/getdemand/header.php
@@ -39,17 +39,21 @@
     <script src="<?php echo get_template_directory_uri(); ?>/bootstrap/js/html5shiv.js" type="text/javascript"></script>
     <script src="http://atmosferiq.com/wp-content/themes/starter-theme/bootstrap/js/respond.min.js" type="text/javascript"></script>
 <![endif]-->
-<!-- Analytics -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-42099303-1', 'atmosferiq.com');
-  ga('send', 'pageview');
-
+<script type="text/javascript">
+var _ss = _ss || [];
+_ss.push(['_setDomain', 'https://koi-NTJGC2.sharpspring.com/net']);
+_ss.push(['_setAccount', 'KOI-Q0Q162']);
+_ss.push(['_trackPageView']);
+(function() {
+    var ss = document.createElement('script');
+    ss.type = 'text/javascript'; ss.async = true;
+    ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'koi-NTJGC2.sharpspring.com/client/ss.js?ver=1.1.1';
+    var scr = document.getElementsByTagName('script')[0];
+    scr.parentNode.insertBefore(ss, scr);
+})();
 </script>
+
 <?php if (is_page('contact') ){ ?>
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyABCwJzMiFzDbmhvNS-VDW94Ve1woyKUTs&sensor=true"></script>
   <?php } ?>
